8780eecc36c7f6bfe8ad261a31dc14dcc4970f37
Richard Bultitude <richardbultitude@TGG-LAP-206220.broadband>
2021-03-18 19:55:55 +0000

    install and configure http-server. update sketch to es6 syntax

--- sketch.js
+++ sketch.js
@@ -1,60 +1,63 @@
-var myCanvas;
-var shapeArr = [];
+let myCanvas;
+const shapeArr = [];
 //set up grid
-var shapeSetWidth;
-var xOffset;
-var shapeVspread;
-var shapeHspread;
-var boxWidth;
-var boxHeight;
-var num = 2;
-var spacingG;
+let shapeSetWidth;
+let xOffset;
+let shapeVspread;
+let shapeHspread;
+let boxWidth;
+let boxHeight;
+let num = 2;
+let spacingG;
 
-function Shape(xPos, yPos, radius, alpha) {
-	this.xPos = xPos;
-	this.yPos = yPos;
-	this.origYPos = yPos;
-	this.radius = radius;
-	this.origRadius = radius;
-	this.thirdRadius = this.radius/3;
-	this.randomNo = random(1,10);
-	this.fillColour = color(255,255,255,alpha);
+class Shape {
+  constructor (xPos, yPos, radius, alpha) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.origYPos = yPos;
+    this.radius = radius;
+    this.origRadius = radius;
+    this.thirdRadius = this.radius/3;
+    this.randomNo = random(1,10);
+    this.fillColour = color(255,255,255,alpha);
+  }
+
+  paint() {
+    noStroke();
+    fill(this.fillColour);
+    ellipse(this.xPos, this.yPos, this.radius, this.radius);
+  };
+
+  update() {
+    const newXPos = Math.floor(frameCount / 500);
+
+    //move forward if inside box
+    if (this.xPos < boxWidth + spacingG / 12) {
+      this.xPos = this.xPos += 1;
+
+      if (this.xPos >= boxWidth/3) {
+        const yNoise = noise(frameCount/500) * this.randomNo;
+        //when moving reduce size
+        if (this.radius > 1) {
+          this.radius -= 0.015;
+        }
+        if (this.yPos > height/2) {
+          this.yPos -= 0.5 * yNoise;
+        }
+        else if (this.yPos <= height) {
+          this.yPos += 0.5 * yNoise;
+        }
+      }
+    }
+    //or return to start
+    else {
+      this.xPos = 0 - spacingG / 12;
+      this.yPos = this.origYPos;
+      this.radius = this.origRadius;
+    }
+  };
 }
 
-Shape.prototype.paint = function() {
-	noStroke();
-	fill(this.fillColour);
-	ellipse(this.xPos, this.yPos, this.radius, this.radius);
-};
-
-Shape.prototype.update = function() {
-	var newXPos = Math.floor(frameCount / 500);
-
-	//move forward if inside box
-	if (this.xPos < boxWidth + spacingG / 12) {
-		this.xPos = this.xPos += 1;
-
-		if (this.xPos >= boxWidth/3) {
-			var yNoise = noise(frameCount/500) * this.randomNo;
-			//when moving reduce size
-			if (this.radius > 1) {
-				this.radius -= 0.015;
-			}
-			if (this.yPos > height/2) {
-				this.yPos -= 0.5 * yNoise;
-			}
-			else if (this.yPos <= height) {
-				this.yPos += 0.5 * yNoise;
-			}
-		}
-	}
-	//or return to start
-	else {
-		this.xPos = 0 - spacingG / 12;
-		this.yPos = this.origYPos;
-		this.radius = this.origRadius;
-	}
-};
 
 function createShapes(spacing) {
 	spacingG = spacing;
@@ -76,7 +79,7 @@ function createShapes(spacing) {
 			var newShapeBottom;
 			var alphaBottom = 255 - k * (colorOffset * 2);
 			newShapeBottom = new Shape(i * spacing + xOffset, boxHeight/2 + k * spacing + xOffset, spacing / 2, alphaBottom);
-			shapeArr.push(newShapeBottom);	
+			shapeArr.push(newShapeBottom);
 		}
 	}
 }
